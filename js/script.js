c74180e8e3cdff331ed02817278d7c610c27d056
Alex <36799721+MiSTeR1995@users.noreply.github.com>
2024-11-27 20:40:14 +0300

    first init

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// document.addEventListener('scroll', function () {
+//     const parallaxElements = document.querySelectorAll('[data-parallax]');
+//     const scrollY = window.scrollY;
+
+//     parallaxElements.forEach((element) => {
+//         const speed = 0.3; // Скорость параллакса
+//         const offset = scrollY * speed;
+
+//         const image = element.querySelector('img');
+//         if (image) {
+//             image.style.transform = `translateY(${offset}px)`;
+//         }
+//     });
+// });
